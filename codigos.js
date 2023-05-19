3a07a7d8d4135baf54b25e7fae6928cfed6504a6
gabrieeelll <gabrielzaomoura@gmail.com>
2023-05-19 08:57:31 -0300

    first commit

---
+++ codigos.js
@@ -0,0 +1,22 @@
+
+
+
+function ativarNome(){
+    let nome = 'GAAB';
+    console.log(nome)
+     
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+
+function somarNumeros(){
+    let a = 6;
+    let b = 8;
+    let c = a*b;
+
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
